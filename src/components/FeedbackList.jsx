c3e742269beee04fea70cc25a22c31e13f9998bf
Alexander Popov <shurikation@gmail.com>
2022-03-10 13:52:42 +0400

    Finish chapter #4.6

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,24 +1,49 @@
 import FeedbackItem from './FeedbackItem'
-import PropTypes from 'prop-types'; 
+import PropTypes from 'prop-types';
+import { motion, AnimatePresence } from 'framer-motion'
 
-function FeedbackList({feedback, handleDelete}) {
-  if(!feedback || feedback.length === 0) {
+function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
     return <p>No feedback yet...</p>
   }
 
+
   return (
     <div className='feedback-list'>
-      {
-        feedback.map((item) => (
-            <FeedbackItem 
-            key={item.id} 
-            item={item} 
-            handleDelete={handleDelete}
-            />
-        ))
-      }
+      <AnimatePresence>
+        {
+          feedback.map((item) => (
+            <motion.div 
+            key={item.id}
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+            >
+              <FeedbackItem
+                key={item.id}
+                item={item}
+                handleDelete={handleDelete}
+              />
+            </motion.div>
+          ))
+        }
+      </AnimatePresence>
     </div>
   )
+
+  // return (
+  //   <div className='feedback-list'>
+  //     {
+  //       feedback.map((item) => (
+  //           <FeedbackItem 
+  //           key={item.id} 
+  //           item={item} 
+  //           handleDelete={handleDelete}
+  //           />
+  //       ))
+  //     }
+  //   </div>
+  // )
 }
 
 FeedbackList.propTypes = {
