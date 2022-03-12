5fa55536101d6711e060f93e086cdc9ece001568
Alexander Popov <shurikation@gmail.com>
2022-03-12 18:47:59 +0400

    Finish #6.5 - Side Effects with UseEffects

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,59 +1,78 @@
-import { useState, useContext } from 'react'
-import RatingSelect from './RatingSelect';
+import { useState, useContext, useEffect } from 'react'
+import RatingSelect from './RatingSelect'
 import Card from './shared/Card'
-import Button from './shared/Button';
-import FeedbackContext from '../context/FeedbackContext';
+import Button from './shared/Button'
+import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackForm() {
-  const [text, setText] = useState('');
-  const [btnDisabled, setBtnDisabled] = useState(true);
-  const [message, setMessage] = useState('');
-  const [rating, setRating] = useState('');
+  const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
 
-  const {addFeedback} = useContext(FeedbackContext);
+  const { addFeedback, feedbackEdit } = useContext(FeedbackContext)
+
+  useEffect(() => {
+    console.log('YYYYEEE', feedbackEdit.edit)
+    console.log('YYYYEEE', feedbackEdit)
+
+    
+    if (feedbackEdit.edit === true) {
+      setBtnDisabled(false)
+      setText(feedbackEdit.item.text)
+      setRating(feedbackEdit.item.rating)
+      
+    }
+  }, [feedbackEdit])
 
   const handleTextChange = (e) => {
-      if(text === '') {
-        setBtnDisabled(true);
-        setMessage(null)
-      } else if(text !== '' && text.trim().length <= 10) {
-        setBtnDisabled(true);
-        setMessage('Text must be at least 10 chars!')
-      } else {
-        setBtnDisabled(false);
-        setMessage(null);
-      }
+    console.log('TEXT', text)
+    if (text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if (text !== '' && text.trim().length <= 10) {
+      setMessage('Text must be at least 10 characters')
+      setBtnDisabled(true)
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
 
-    setText(e.target.value);
-  } 
+    setText(e.target.value)
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    if(text.trim().length > 10) {
+    e.preventDefault()
+    if (text.trim().length > 10) {
       const newFeedback = {
-        text: text,
-        rating: rating
+        text,
+        rating,
       }
 
-      addFeedback(newFeedback);
-      setText('');
+      addFeedback(newFeedback)
+
+      setText('')
     }
   }
 
   return (
     <Card>
       <form onSubmit={handleSubmit}>
-        <h2>How do you rate?</h2>
-        <RatingSelect select={(rating) => setRating(rating)}/>
-        <div className="input-group">
-          <input 
-            type="text" placeholder='Write a review'
-            onChange={(e) => handleTextChange(e)}
-            value={text}/>
-          <Button type="submit" isDisabled={btnDisabled}>Send</Button>
+        <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
+        <div className='input-group'>
+          <input
+            onChange={handleTextChange}
+            type='text'
+            placeholder='Write a review'
+            value={text}
+          />
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
 
-        { message && <div className='message'>{message}</div>}
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   )
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,41 +1,66 @@
-import { createContext, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'
+import { createContext, useState } from 'react'
 
-
-//This is a provider
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: 'Context!',
-      rating: 10
-    }
-  ]);
+      text: 'This is feedback item 1',
+      rating: 10,
+    },
+    {
+      id: 2,
+      text: 'This is feedback item 2',
+      rating: 9,
+    },
+    {
+      id: 3,
+      text: 'This is feedback item 3',
+      rating: 7,
+    },
+  ])
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  })
 
+  // Add feedback
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
+
+  // Delete feedback
   const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure?')) {
+    if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
 
-
-  const addFeedback = (newFeedback) => {
-    //uuidv4 helps generate uniq id for feedback:
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  // Set item to be updated
+  const editFeedback = (item) => {
+    console.log('ITEM IN FUNC', item)
+    setFeedbackEdit({
+      item,
+      edit: true,
+    })
   }
 
   return (
-    <FeedbackContext.Provider value={{
-      feedback,
-      deleteFeedback,
-      addFeedback
-    }}>
+    <FeedbackContext.Provider
+      value={{
+        feedback,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        feedbackEdit,
+      }}
+    >
       {children}
     </FeedbackContext.Provider>
   )
 }
 
-export default FeedbackContext;
+export default FeedbackContext
